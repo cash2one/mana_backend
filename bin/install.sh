ad6b4a31e074e36322166401f79009ce67eaa45d
root <root@ywy.(none)>
2015-04-21 19:42:55 +0800

    fix script

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-rm -fr /usr/lib/python2.*/mana_backend
+rm -fr /usr/lib/python2.*/dist-packages/mana_backend
+rm -fr /usr/lib/python2.*/site-packages/mana_backend
 rm -fr /usr/local/bin/mana-alarm 
 rm -fr /etc/init.d/mana-backend
-cp -r ../mana_backend /usr/lib/python2.*/
+cp -r ../mana_backend /usr/lib/python2.*/dist-packages/
+cp -r ../mana_backend /usr/lib/python2.*/site-packages/
 cp mana-alarm /usr/local/bin/mana-alarm
 cp mana-backend /etc/init.d/mana-backend
--- bin/remove.sh
+++ bin/remove.sh
@@ -2,6 +2,7 @@
 
 #rm -fr /etc/mana
 #rm -fr /var/log/mana
-rm -fr /usr/lib/python2.*/mana_backend
+rm -fr /usr/lib/python2.*/dist-packages/mana_backend
+rm -fr /usr/lib/python2.*/site-packages/mana_backend
 rm -fr /usr/local/bin/mana-alarm 
 rm -fr /etc/init.d/mana-backend
