aa8d5e087bd0ebfe7f15be8aea7e51afe2250879
root <root@ywy.(none)>
2015-04-22 11:19:29 +0800

    fix script for python header path

--- bin/install.sh
+++ bin/install.sh
@@ -3,7 +3,13 @@ rm -fr /usr/lib/python2.*/dist-packages/mana_backend
 rm -fr /usr/lib/python2.*/site-packages/mana_backend
 rm -fr /usr/local/bin/mana-alarm 
 rm -fr /etc/init.d/mana-backend
-cp -r ../mana_backend /usr/lib/python2.*/dist-packages/
-cp -r ../mana_backend /usr/lib/python2.*/site-packages/
+python_path1="/usr/lib/python2.*/dist-packages/"
+python_path2="/usr/lib/python2.*/site-packages/"
+if [ -d $python_path1 ];then
+    cp -r ../mana_backend /usr/lib/python2.*/dist-packages/
+fi
+if [ -d $python_path2 ];then
+    cp -r ../mana_backend /usr/lib/python2.*/site-packages/
+fi
 cp mana-alarm /usr/local/bin/mana-alarm
 cp mana-backend /etc/init.d/mana-backend
